e46be97ac22cd7729934b48db578337fb31c76a4
gabhu <gabhu@hotmail.es>
2019-06-05 08:40:47 -0500

    subiendo proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/* Acá va tu código */
+
+const empezar= document.getElementById('empezar')
+empezar.addEventListener('click',()=>{
+    let pasi=document.getElementById('password').value;
+if (pasi==='LABORATORIA'){
+document.getElementById('pantalla-1').classList.add('hide')
+document.getElementById('pantalla-2').classList.remove('hide')
+}
+else if(document.getElementById('resultado').innerHTML='error');
+
+else{document.getElementById('resultado').innerHTML='vuelve a intentarlo mas tarde'}});
+
+const seguir=document.getElementById('seguir')
+seguir.addEventListener('click',()=>{
+    document.getElementById('pantalla-2').classList.add('hide')
+    document.getElementById('pantalla-3').classList.remove('hide')
+})
