20d1fdd9b17800a9317ff735735cd7f90b22805b
gabhu <gabhu@hotmail.es>
2019-06-07 08:59:52 -0500

    cambios en css,js,html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-
-const empezar= document.getElementById('empezar')
-empezar.addEventListener('click',()=>{
-    let pasi=document.getElementById('password').value;
-if (pasi==='LABORATORIA'){
-document.getElementById('pantalla-1').classList.add('hide')
-document.getElementById('pantalla-2').classList.remove('hide')
-}
-else if(document.getElementById('resultado').innerHTML='error');
-
-else{document.getElementById('resultado').innerHTML='vuelve a intentarlo mas tarde'}});
-
+let contador=0;
+  const empezar=document.getElementById('empezar')
+  empezar.addEventListener('click',()=>{
+    
+/*INTENTO VALIDO*/
+if(contador<=3){
+let password=document.getElementById('password').value
+        if(password==='LABORATORIA'){
+            document.getElementById('pantalla-1').classList.add('hide');
+            document.getElementById('pantalla-2').classList.remove('hide');
+        }
+    /*1ER INTENTO NO VALIDO*/
+        else{
+            document.getElementById('resultado').innerHTML='vuelve a intentar';
+            contador=contador+1;
+            /*2DO INTENTO NO VALIDO Y TERCERO*/
+        if(contador>2){
+            document.getElementById('resultado').innerHTML='vuelve a intentarlo mas tarde';
+            document.getElementById('password').disabled=true; 
+            document.getElementById('empezar').disabled=true;
+            }}}}
+            )
 const seguir=document.getElementById('seguir')
 seguir.addEventListener('click',()=>{
-    document.getElementById('pantalla-2').classList.add('hide')
-    document.getElementById('pantalla-3').classList.remove('hide')
-})
+document.getElementById('pantalla-2').classList.add('hide');
+document.getElementById('pantalla-3').classList.remove('hide');
+
+  });
+
+
+
+
+
+
+
+
+
+
