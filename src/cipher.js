ccefe6f135ebe76d0e5168025f2a3298b1625269
gabhu <gabhu@hotmail.es>
2019-06-11 12:42:10 -0500

    cambio en cipher y creacion de pantalla 4

--- src/cipher.js
+++ src/cipher.js
@@ -3,21 +3,43 @@ window.cipher = {
     /* Acá va tu código que cifra*/
     string=string.toUpperCase();
     let confesion_2='';
-    for(let i=0; i<string.length; i++){
+    for(let i=0; i<string.length; i++)
+     { let caracter = string[i];
         let candado=string.charCodeAt(i);
-    if(candado>=65 && candado<=90){
-    confesion_2 +=String.fromCharCode((string.charCodeAt(i)-65+parseInt(offset))%26+65);
-}
-    else{
-      (candado>=97 && candado<=122);
-      confesion_2 +=String.fromCharCode((string.charCodeAt(i)-65+parseInt(offset))%26+97);
+        if(candado>=65 && candado<=122)
+        {
+        confesion_2 +=String.fromCharCode((candado-65+parseInt(offset))%26+65);
+        }
+         else {
+           confesion_2+=caracter;
+
+         }     
+    
     }
-  }
-return confesion_2
+  
+return confesion_2;
 },
   
   
  decode: (offset, string) => {
     /* Acá va tu código que descifra*/
- }
-};
+    string=string.toUpperCase();
+    let confesion_3='';
+    for(let i=0; i<string.length; i++)
+     { let caracter = string[i];
+        let candado=string.charCodeAt(i);
+        if(candado>=65 && candado<=122)
+        {
+        confesion_3 +=String.fromCharCode((candado+65-parseInt(offset))%26+65);
+        }
+         else {
+           confesion_3+=caracter;
+
+         }     
+    
+    }
+  
+return confesion_3;
+},
+}
+
--- src/index.js
+++ src/index.js
@@ -32,6 +32,36 @@ let password=document.getElementById('password').value
                   let cifra=document.getElementById('confesion').value;
                   document.getElementById('traduccion').innerHTML=window.cipher.encode(age,cifra);
               });
+              const descifrar=document.getElementById('descifrar')
+              descifrar.addEventListener('click',()=>{
+                  let age=document.getElementById('offset').value;
+                  let descifra=document.getElementById('confesion').value;
+                  document.getElementById('traduccion').innerHTML=window.cipher.decode(age,descifra);
+              });
+             const limpiar=document.getElementById('limpiar')
+              limpiar.addEventListener('click',()=>{
+                  document.getElementById('offset').value='';
+                  document.getElementById('confesion').value='';
+                  document.getElementById('traduccion').value='';
+              });
+              const publicar=document.getElementById('publicar')
+              publicar.addEventListener('click',()=>{
+                document.getElementById('pantalla-3').classList.add('hide');
+                document.getElementById('pantalla-4').classList.remove('hide');
+
+                let desp = document.getElementById('offset').value;
+                let mensaje = document.getElementById('traduccion').value;
+                let nombre=document.getElementById('nombre').value;
+                document.getElementById('traducto').innerHTML=  `${nombre} ${desp} `
+                document.getElementById('traducto-2').innerHTML=`${mensaje}`
+
+                 
+            }
+
+
+
+              )
+              
               
 
 
